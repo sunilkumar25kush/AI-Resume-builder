30cf953744710e2c0a8e503e00bd7f9e87d1af9f
sunilkumar25kush <sunilkumar25@navgurkul.org>
2026-08-06 11:47:21 +0530

    Remove unused get version API; add Google OAuth env vars to render.yaml

--- client/src/api/versions.ts
+++ client/src/api/versions.ts
@@ -7,13 +7,6 @@ export const versionsApi = {
     return res.data.data.versions;
   },
 
-  async get(resumeId: string, versionId: string): Promise<ResumeVersion> {
-    const res = await apiClient.get<ApiEnvelope<{ version: ResumeVersion }>>(
-      `/resumes/${resumeId}/versions/${versionId}`,
-    );
-    return res.data.data.version;
-  },
-
   async restore(resumeId: string, versionId: string): Promise<Resume> {
     const res = await apiClient.post<ApiEnvelope<{ resume: Resume }>>(`/resumes/${resumeId}/versions/${versionId}/restore`);
     return res.data.data.resume;
